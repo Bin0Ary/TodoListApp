0bec2fa9a798686a2d78f19f0c72aff7e9599c92
Bin0Ary <120618010+Bin0Ary@users.noreply.github.com>
2025-11-13 02:14:08 +0330

    Added IServices and Services(Empty)

---
+++ TodoListLibrary/Services/IServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoListLibrary.Models;
+
+namespace TodoListLibrary.Services
+{
+    public interface IServices
+    {
+        void CreateTodoList(string path, List<TodoListItemModel> list);
+        void CreateTodoItem(string path, List<TodoListItemModel> list);
+        void DeleteTodoList(string path);
+        void DeleteTodoItem(string path, List<TodoListItemModel> list);
+        void EditTodoList(string path, string newName);
+        void EditTodoItem(string path, List<TodoListItemModel> list, TodoListItemModel model);
+
+    }
+}
---
+++ TodoListLibrary/Services/Services.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoListLibrary.Models;
+
+namespace TodoListLibrary.Services
+{
+    public class Services : IServices
+    {
+        void IServices.CreateTodoItem(string path, List<TodoListItemModel> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IServices.CreateTodoList(string path, List<TodoListItemModel> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IServices.DeleteTodoItem(string path, List<TodoListItemModel> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IServices.DeleteTodoList(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IServices.EditTodoItem(string path, List<TodoListItemModel> list, TodoListItemModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IServices.EditTodoList(string path, string newName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
